e18b0737d2ff81d352f7ecfa420f37583a3c4509
Giulia Nickhorn <nickhorngiulia@gmail.com>
2026-06-12 22:37:44 -0300

    Finalização da primeira etapa

--- karaoke/player.js
+++ karaoke/player.js
@@ -4,6 +4,7 @@ const { sleep } = require('./utils');
 const { Musica } = require('./musica');
 const { Parte } = require('./parte');
 
+
 const boompala = new Musica('BOOMPALA', 'LE SSERAFIM');
 const macarena1 = `Dale a tu cuerpo alegría, Macarena \n Que tu cuerpo es pa' darle alegría y cosa buena`;
 const macarena2 = `Dale a tu cuerpo alegría, Macarena \n Eh, Macarena (¡ay!)`;
@@ -23,47 +24,118 @@ const holdinOnToo = `Holdin' on too tight, gotta let it flow \n Feelin' like a w
 const permanentIs = `Permanent is something that I'm lettin' go \n I gotta let it go, yeah`;
 const imGoing = `I'm goin', goin', goin', goin', goin', goin' crazy, yeah \n I couldn't be more in touch if you paid me`;
 const selflove = `Self-love is the mommy, I'm the baby \n Zen out, meditate on the daily`;
-const everythingsWater = `Everything's water and everything's air`;
-const boompala1 = `Uh, boompala, boompala, boompala, ayy`;
-const everythingsEmpty = `Everything's empty so everything's clear`;
+const everythingsWater = `Everything's water and everything's air \n Uh, boompala, boompala, boompala, ayy`;
+const everythingsEmpty = `Everything's empty so everything's clear \n Uh, boompala, boompala, boompala, ayy`;
 
 //verificar partes faltantes e criar.
 //começa a adicionar as partes da música, com letra, tempo e tag1
 function estrofeMacarena(){  
     boompala.addParte(
-        new Parte(macarena1, 7000, 'verso1')
+        new Parte(macarena1, 6000, 'verso')
     );
     boompala.addParte(
-        new Parte(macarena2, 5000, 'verso2')
+        new Parte(macarena2, 6000, 'verso')
     );
 }
 estrofeMacarena();
 // silêncio de 3s
 boompala.addParte(
-    new Parte (sserafimBaby, 8000, 'verso3')
+    new Parte (sserafimBaby, 8000, 'verso')
 )
-function refrao(){
+function parteBoompala(){
     boompala.addParte(
-        new Parte (boompala2, 4000, 'verso4')
-    )
+        new Parte (boompala2, 4000, 'refrao')
+    )}
+function refrao1(){
+    parteBoompala();
     boompala.addParte(
-        new Parte (youCantHoldOn, 5000, 'verso5')
+        new Parte (youCantHoldOn, 5000, 'refrao1')
     )
 }
-refrao();
+refrao1();
+
 boompala.addParte(
-    new Parte (wakeUpsayin, 5000, 'verso6')
+    new Parte (wakeUpsayin, 5000, 'verso')
 )
 boompala.addParte(
-    new Parte (babySayGoodbye, 4000, 'verso7')
+    new Parte (babySayGoodbye, 4000, 'verso')
 )
 boompala.addParte(
-    new Parte (beatinUpAll, 5000, 'verso8')
+    new Parte (beatinUpAll, 5000, 'verso')
 )
 boompala.addParte(
-    new Parte (pauseForASecond, 3000, 'verso9') //46
+    new Parte (pauseForASecond, 4000, 'verso')
 )
-refrao();
+
+refrao1();
+
+function refrao2(){
+    parteBoompala();
+    boompala.addParte(
+        new Parte (nothingsForever, 5000, 'refrao2')
+    )
+}
+refrao2();
+
+function estrofeOnMyChest(){
+    boompala.addParte(
+        new Parte (onMyChest1, 5000, 'verso')
+    )
+    boompala.addParte(
+        new Parte (myCelestial1, 4000, 'verso')
+    )
+    boompala.addParte(
+        new Parte (onMyChest2, 4000, 'verso')
+    )
+    boompala.addParte(
+        new Parte (myCelestial2, 5000, 'verso')
+    )
+}
+estrofeOnMyChest();
+
+boompala.addParte(
+        new Parte (holdinOnToo, 5000, 'verso')
+    )
+    boompala.addParte(
+        new Parte (permanentIs, 4000, 'verso')
+    )
+    boompala.addParte(
+        new Parte (imGoing, 4000, 'verso')
+    )
+    boompala.addParte(
+        new Parte (selflove, 5000, 'verso')
+    )
+
+boompala.addParte(
+    new Parte (boompala2, 5000, 'verso')
+)
+boompala.addParte(
+    new Parte (everythingsWater, 4000, 'verso')
+)
+
+boompala.addParte(
+    new Parte (boompala2, 5000, 'verso')
+)
+boompala.addParte(
+    new Parte (everythingsEmpty, 4000, 'verso')
+)
+
+estrofeOnMyChest();
+
+refrao1();
+refrao2();
+
+function estrofeMacarena2(){  
+    boompala.addParte(
+        new Parte(macarena1, 4000, 'verso')
+    );
+    boompala.addParte(
+        new Parte(macarena2, 4500, 'verso')
+    );
+}
+estrofeMacarena2();
+estrofeMacarena2();
+
 
 async function play() {
     try {
@@ -71,7 +143,7 @@ async function play() {
         //ex.:
         for (const parte of boompala.partes) {
             //imprime parte e letra
-            console.log( " -- " + parte.tag + " --" );
+            console.log( " -- --" );
             console.log( "> " + parte.letra );
             //agurda o tempo para a letra
             await sleep( parte.tempoEspera );

64abfc4356b0e7190769dbd0fc33a3c1950c171d
nickhorngiulia-alt <nickhorngiulia@gmail.com>
2026-06-23 15:28:45 -0300

    aula 23-06 final

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//importações dos objetos locais
+const {musica, play} = require('./karaoke/player');
+console.log(`Título da música: ${musica}`)
+
 // 1. Importar o Express
 const express = require('express');
 
@@ -22,8 +26,54 @@ app.post('/testePost', (req,res) => {
 
 } );
 
+/*
+*API da música
+* GET /musica/:id -> retornar a música inteira
+* GET /musica/id/nome -> nome da música
+* GET  /musica:id/:parte -> retornar parte da música
+*/
+//GET /musica/:id -> retornar a música inteira
+app.get('/musica/:id', (req, res) => {
+    const id = req.params.id;
+    if(id != 0){
+        res.status(404);
+        res.send('Música não encontrada.')
+    }
+    const musicaJSON = json.stringfy(musica); // transformar js em string json
+    //res.JSON(musicaJSON);
+    res.contentType('application/json');
+    res.send(musicaJSON);
+})
+
+// GET /musica/:id/nome -> nome da música
+app.get('/musica/:id/titulo', (req, res) => {
+    if(id != 0){
+        res.status(404);
+        res.send('Música não encontrada.')
+    }
+    res.send({titulo: musica.nome});
+})
+
+//GET  /musica:id/:parte -> retornar parte da música
+app.get('/musica/:id/parte/:parte'), (req, res) => {
+    const id = req.params.id;
+    const parte = req.params.parte;
+    if(id != 0){
+        res.status(404);
+        res.send('Música não encontrada.')
+    }
+    const parteParaRetornar = musica.partes[parte];
+    res.contentType('application/json')
+    red.send(JSON.stringfy(parteParaRetornar));
+}
+
+//GET /musica/:id -> retornar a música inteira
+app.get('/player/:id/play', (req, res) => {
+    play();
+    res.send('Música reproduziu no servidor!')
+})
 
 // 5. Abrir a loja (escutar a porta)
-app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000');
+app.listen(5500, () => {
+    console.log('Servidor rodando em http://localhost:5500');
 });
